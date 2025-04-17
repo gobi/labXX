cfc286a2245551f3178cc993d7b80de0d0dd861f
gobi <gobi.mn@gmail.com>
2025-04-17 12:51:39 +0800

    asdfasdf

--- LabXXMain.java
+++ LabXXMain.java
@@ -2,5 +2,8 @@ public class LabXXMain {
 	public static void main(String[] args) {
 		// Create an instance of the LabXX class
 		LabXX lab = new LabXX();
+		// asdfasd\
+		// asdfasd
+		// asdfasdfadsf asdfas
 	}
 }
